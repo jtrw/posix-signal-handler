186106992d1c2e2ba93944d3290ae108cbb9f55e
Nil Borodulia <nil.b@oxtech.org>
2022-09-11 21:18:09 +0100

    add docker container

--- tests/Src/SignalHandlerTest.php
+++ tests/Src/SignalHandlerTest.php
@@ -10,17 +10,22 @@ class SignalHandlerTest extends TestCase
     public function testTerminate()
     {
         $pid = exec('php -q '.__DIR__.'/command/runScript.php > /dev/null 2>&1 & echo $!;');
-    
-        exec("ps -p ".$pid, $output);
-        Assert::assertNotEmpty($output[1]);
+        $isRunning = shell_exec("pgrep -f runScript.php");
+        //exec("ps -p ".$pid, $output);
+        //Assert::assertNotEmpty($output[1]);
+        Assert::assertNotEmpty($isRunning);
         sleep(1);
         $cmd = sprintf("kill -%s %s", SIGILL, $pid);
         exec($cmd);
         //posix_kill($pid, SIGILL);
-        $output = [];
-        exec("ps -p ".$pid, $output);
-
-        Assert::assertArrayNotHasKey(1, $output);
+        $isRunning = shell_exec("pgrep -f runScript.php");
+        //exec("ps -p ".$pid, $output);
+        //Assert::assertNotEmpty($output[1]);
+        Assert::assertNull($isRunning);
+//        $output = [];
+//        exec("ps -p ".$pid, $output);
+//
+//        Assert::assertArrayNotHasKey(1, $output);
     }
     
 }
